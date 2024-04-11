c0f10f08b23524d7a2c621645e4e877fee7e46c4
Minjeong Kim <alswjd7461@gmail.com>
2024-04-11 21:23:11 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if (a%2==1 && b%2==1)
+        answer=a*a+b*b;
+    else if ((a%2==1 && b%2==0)||(a%2==0 && b%2==1))
+        answer=2*(a+b);
+    else 
+    answer=a-b;
+    
+    if (answer<0)
+        answer=answer*(-1);
+    
+    return answer;
+}
