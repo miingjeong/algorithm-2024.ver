91dd4de1b770d6bfac87fe9f4fb43737d38bff3c
Minjeong Kim <alswjd7461@gmail.com>
2025-07-01 22:30:22 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -6,12 +6,12 @@ using namespace std;
 int solution(int num, int k) {
     int answer = -1;
     string s=to_string(num);
-    char target=k+'0'; // 정수 k 를 문자로 변환...
-    for(int i=0; i<s.size(); i++){
-        if (target==s[i]) 
+    char target=k+'0';
+    for(int i=0; i<s.size(); i++)
+    {
+        if (s[i]==target){
             return answer=i+1;
     }
-        
-    
+    }
     return answer;
 }
