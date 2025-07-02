ccec1409d711095439f60e528da8868405769fa9
Minjeong Kim <alswjd7461@gmail.com>
2025-07-02 21:49:51 +0900

    [level 2] Title: 구명보트, Time: 1.82 ms, Memory: 4.71 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 4.64 MB, 시간: 1.48 ms
+메모리: 4.71 MB, 시간: 1.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 23:39:17
+2025년 07월 02일 21:49:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -4,27 +4,18 @@
 using namespace std;
 
 int solution(vector<int> people, int limit) {
-
-    sort(people.begin(), people.end());
-    int i = 0;
-    int j = people.size() - 1;
-
     int answer = 0;
-
-    while(i <= j)
-    {
-        if(people[i] + people[j] <= limit)
-        {
-            ++i;
-            --j;
+    int left=0; // 가장 작은 사람
+    int right=people.size() -1 ;// 가장 무거운 사람
+    
+    sort(people.begin(), people.end());
+    while(left <= right){
+        if (people[left] + people[right] <=limit){
+            left ++;
         }
-        else
-        {
-            --j;
-        }
-
-        ++answer;
+        right --;
+        answer++;
     }
-
+    
     return answer;
 }
